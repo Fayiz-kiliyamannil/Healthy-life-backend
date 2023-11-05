f2b0a755a729555a64e896869c02bb1005e2af0f
fayiz kiliyamannil <fayiz.kiliyamannil@gmail.com>
2023-11-06 00:50:55 +0530

    imp-socket.io not complected

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const path = require('path')
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB)
-.then(()=>console.log('mongodbConnected...'))
-.catch((error)=>console.error('error in MongoDB'))
+    .then(() => console.log('mongodbConnected...'))
+    .catch((error) => console.error('error in MongoDB'))
 
-const app= express();
+const app = express();
+
 
 
 
@@ -24,14 +25,55 @@ const adminRoute = require('./Routes/adminRoute')
 const trainerRoute = require('./Routes/trainerRoute');
 const errorHandler = require('./Middlewares/errorHandler');
 
-app.use("/user",userRoute);
-app.use('/admin',adminRoute);
-app.use('/trainer',trainerRoute); 
- 
+app.use("/user", userRoute);
+app.use('/admin', adminRoute); 
+app.use('/trainer', trainerRoute);
 
 
 app.use(errorHandler)
-const port  = process.env.PORT || 5001;
-app.listen(port,()=>{
+ 
+
+const port = process.env.PORT || 5001;
+const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
-})    
+}) 
+
+ const io = require('socket.io')(server,{
+    pingTimeout: 60000,
+    cors:{
+        origin:'http://localhost:3000'
+    }
+ })
+
+ io.on('connection',(socket)=>{
+    console.log('connected to socket.io.......!');
+
+    socket.on('user-setup',(userData)=>{
+        socket.join(userData?._id);
+        console.log("userId---------usesr-setup",userData?._id);
+        socket.emit('connected..!')
+    })
+
+    socket.on('setup',(trainerData)=>{
+        socket.join(trainerData?.trainer);
+        console.log(trainerData?.trainer,'trainer--Id');
+        socket.emit('connected..!')
+    })    
+
+    socket.on('joinChat',(room)=>{
+        socket.join(room);
+        console.log(room,'user join in rooom---');
+    })
+
+    socket.on('submitChat',(newChat)=>{
+        console.log(newChat,'newchat');
+        socket.emit('messageRecived',newChat)
+        
+    }) 
+
+
+ })
+ 
+
+  
+  
