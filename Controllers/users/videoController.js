d9ad0345b29897bf00a289bcfd5f2186d3ee7171
fayiz kiliyamannil <fayiz.kiliyamannil@gmail.com>
2023-11-14 00:01:22 +0530

    some modification

--- Controllers/users/videoController.js
+++ Controllers/users/videoController.js
@@ -1,12 +1,21 @@
 const video = require('../../Models/videoModel');
 const user = require('../../Models/userModel');
+const moment = require('moment');
+const date = moment();
+const toDayDate = date.format('DD-MM-YYYY');
+const Order = require('../../Models/orderModel');
 
 const getUsersVideos = async (req, res, next) => {
     try {
         const userDetails = await user.findOne({ _id: req.body.userId })
-        const getVideos = await video.find({ trainerId: userDetails.trainer })
-        return res.status(200).send({ message: 'fetch-users-video-success', success: true, getVideos })
+        const getVideos = await video.find({ trainerId: userDetails.trainer });
+        const orderDetails = await Order.findOne({$and:[{userId:userDetails._id},{status:"Success"}]});
+        if(orderDetails.proEndIn >= toDayDate){
+            return res.status(200).send({ message: 'fetch-users-video-success', success: true, getVideos,proUser:true })
+        }
+        return res.status(200).send({ message: 'fetch-users-video-success', success: true })
     } catch (error) {
+        console.error(error.message);
         next(error)
     }
 }
